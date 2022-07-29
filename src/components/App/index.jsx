1dcd5862d2b557e060ea2353985625f5eee6a8f4
SerBEEan <sergey.atwork7@gmail.com>
2022-07-30 00:08:10 +0300

    init

---
+++ src/components/App/index.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div style={{ fontSize: '1rem' }}>
+        hello world
+      </div>
+      <div style={{ fontSize: '16px' }}>
+        hello world
+      </div>
+    </>
+  );
+}
+
+export default App;
