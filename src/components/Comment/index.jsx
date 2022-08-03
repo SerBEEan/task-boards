6ae5d686085390da9c6b77d103b69c8dc1b465b0
SerBEEan <sergey.atwork7@gmail.com>
2022-08-03 23:22:43 +0300

    изменил Comment

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -4,15 +4,25 @@ import {ReactComponent as IconClose} from '../../Icons/close.svg';
 
 import styles from './styles.module.css';
 
-export default function Comment({ author, content }) {
+export default function Comment({ children, author, onDelete }) {
+    const clickDelete = () => {
+        onDelete?.();
+    };
+
     return (
         <div className={styles.comment}>
             <div className={styles.commentHeader}>
                 {author}
-                <Button type={Type.text} size={Size.xs} shape={Shape.circle} icon={<IconClose />} />
+                <Button
+                    type={Type.text}
+                    size={Size.xs}
+                    shape={Shape.circle}
+                    icon={<IconClose />}
+                    onClick={clickDelete}
+                />
             </div>
             <div className={styles.commentBody}>
-                {content}
+                {children}
             </div>
         </div>
     );
