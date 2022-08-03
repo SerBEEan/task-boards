8f825a3287a866bcccd0620427b27b5f46a8a56a
SerBEEan <sergey.atwork7@gmail.com>
2022-08-04 00:48:32 +0300

    добавил пропс block у Layout

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,13 +1,16 @@
+import classNames from 'classnames';
+
 import styles from './styles.module.css';
 
 export default function Layout(props) {
     const {
         children,
         header,
+        block = false,
     } = props;
 
     return (
-        <div className={styles.layout}>
+        <div className={classNames(styles.layout, {[styles.fullWidth]: block})}>
             {header && <div className={styles.header}>{header}</div>}
             {children}
         </div>
