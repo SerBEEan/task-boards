d3e759eda804ab891215a5956ba70c6110bf4166
SerBEEan <sergey.atwork7@gmail.com>
2022-08-04 00:49:31 +0300

    сделал компонент колонки тикетов

---
+++ src/components/TicketColumn/index.jsx
@@ -0,0 +1,53 @@
+import Border from '../Border';
+import TicketCard, {  } from '../TicketCard';
+import { Color } from '../Tag';
+
+import styles from './styles.module.css';
+
+export default function TicketColumn({ title, button }) {
+    return (
+        <div className={styles.column}>
+            <div className={styles.title}>
+                {title}
+            </div>
+            <Border block>
+                <div className={styles.content}>
+                    <TicketCard
+                        title="Нарисовать иллюстрации"
+                        tags={[
+                            { id: 0, color: Color.violet },
+                            { id: 1, color: Color.mint },
+                            { id: 2, color: Color.red },
+                            { id: 3, color: Color.orange },
+                            { id: 4, color: Color.blue },
+                            { id: 5, color: Color.green },
+                            { id: 6, color: Color.dark },
+                            { id: 7, color: Color.yellow },
+                        ]}
+                        block
+                        hasDescription
+                        hasComment
+                    />
+                    <TicketCard
+                        title="Нарисовать иллюстрации"
+                        tags={[
+                            { id: 0, color: Color.violet },
+                            { id: 1, color: Color.mint },
+                            { id: 2, color: Color.red },
+                            { id: 3, color: Color.orange },
+                            { id: 4, color: Color.blue },
+                            { id: 5, color: Color.green },
+                            { id: 6, color: Color.dark },
+                            { id: 7, color: Color.yellow },
+                        ]}
+                        block
+                        hasDescription
+                        hasComment
+                    />
+
+                    {button}
+                </div>
+            </Border>
+        </div>
+    );
+}
